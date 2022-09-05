2793f80c6bc58e1cda7c8bdbe114e632512950cc
Andrey Weiss <dagmag82@gmail.com>
2022-09-05 23:10:55 +0200

    Main

--- 1 lesson.py
+++ 1 lesson.py
@@ -10,7 +10,7 @@
 # print(type(value))
 
 # s = 'Hello World'
-#print(a, b, s)
+# print(a, b, s)
 # print('{1} - {2} - {0}'. format(a, b, s))
 # print(f'{a} - {b} - {s}')
 
@@ -25,7 +25,248 @@
 # b = int(input())
 # print(a, '+', b, '=', a+b)
 
-a = 8.5456456
-b = 6
-c = round(a * b, 3)
-print(c)
+# a = 8.5456456
+# b = 6
+# c = round(a * b, 3)
+# print(c)
+
+# print('Как тебя зовут?')
+# name = input()
+# print('Привет,', name)
+
+
+# print('Привет, ')
+# name = input()
+# print('Привет, ', name)
+
+# a = input()
+# b = input()
+# c = input()
+# print(a)
+# print(b)
+# print(c)
+
+# print('a', 'b', 'c', sep='*', end='!')
+
+# print('Mercury', 'Venus', sep='*', end='!')
+# print('Mars', 'Jupiter', sep='**', end='?')
+
+# print('a', 'b', 'c', sep='*')
+# print('d', 'e', 'f', sep='**', end='')
+# print('g', 'h', 'i', sep='+', end='%')
+# print('j', 'k', 'l', sep='-', end='\n')
+# print('m', 'n', 'o', sep='/', end='!')
+# print('p', 'q', 'r', sep='1', end='%')
+# print('s', 't', 'u', sep='&', end='\n')
+# print('v', 'w', 'x', sep='%')
+# print('y', 'z', sep='/', end='!')
+
+# language = 'Python'
+# language = 'Pascal'
+# print(language)
+
+# num = int(input())
+# x = 1
+# y = 2
+# print(num, end='\n')
+# print(num + x, end='\n')
+# print(num + y)
+
+# n = int(input())
+# print(n, n + 1, n + 2, sep='\n')
+
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# print(x + y + z)
+
+# sum = int(input()) + int(input()) + int(input())
+# print(sum)
+
+# a = int(input())
+# v = a**3
+# s = 6*a**2
+# print('Объем куба =', v)
+# print('Площадь полной поверхности =', s)
+
+# a = int(input())
+# b = int(input())
+# f = 3*(a+b)**3 + 275*b**2 - 127*a - 41
+# print(f)
+
+# a = int(input())
+# print('Следующее за числом', a, 'число:', a+1)
+# print('Для числа', a, 'предыдущее число:', a-1)
+
+# monitor_cost = int(input())
+# system_unit_cost = int(input())
+# keyboard_cost = int(input())
+# mouse_cost = int(input())
+# number_of_computers = 3
+# print((monitor_cost+system_unit_cost+keyboard_cost+mouse_cost)*number_of_computers)
+
+# sum = int(input()) + int(input()) + int(input()) + int(input())
+# n = 3
+# print(sum*n)
+
+# print((int(input()) + int(input()) + int(input()) + int(input())) * 3)
+
+# a = int(input())
+# b = int(input())
+# print(a, '+', b, '=', a+b)
+# print(a, '-', b, '=', a-b)
+# print(a, '*', b, '=', a*b)
+
+# a = int(input())
+# b = int(input())
+# print(a + b)
+# print(a - b)
+# print(a * b)
+
+# a = int(input())
+# d = int(input())
+# n = int(input())
+# print(a+d*(n-1))
+
+# x = int(input())
+# print(x, x*2, x*3, x*4, x*5, sep='---')
+
+# a = 15 // (16 % 7)
+# b = 34 % a * 5 - 29 % 5 * 2
+# print(a + b)
+
+# a = 82 // 3 ** 2 % 7
+# print(a)
+
+
+# b = int(input())
+# q = int(input())
+# n = int(input())
+# print(b*q**(n-1))
+
+# a = int(input())
+# print(a // 100)
+
+# n = int(input())  # количество школьников
+# k = int(input())  # количество мандарин
+# print(k // n)  # каждому школьнику мандарин
+# print(k % n)  # остаток мандарин в корзине
+
+# people = int(input())
+# print(people // 2 + 1)
+
+# print((int(input())+1)//2)
+
+# a = int(input())
+# print((a+3) // 4)
+
+
+# a = int(input())
+# print(a, 'мин - это', (a // 60), 'час', (a % 60), 'минут.')
+
+# num = int(input())
+# last_digit = num % 10
+# first_digit = num // 10
+# print('Число десятков =', first_digit)
+# print('Число единиц =', last_digit)
+
+# num = int(input())
+# a1 = num // 100
+# a2 = (num // 10) % 10
+# a3 = num % 10
+# print('Сумма цифр =', a1+a2+a3)
+# print('Произведение цифр =', a1*a2*a3)
+
+# num = int(input())
+# a = num // 100
+# b = (num // 10) % 10
+# c = num % 10
+# print(a, b, c, sep='')
+# print(a, c, b, sep='')
+# print(b, a, c, sep='')
+# print(b, c, a, sep='')
+# print(c, a, b, sep='')
+# print(c, b, a, sep='')
+
+
+# num = int(input())
+# a = num // 1000
+# b = (num // 100) % 10
+# c = (num % 100) // 10
+# d = num % 10
+# print('Цифра в позиции тысяч равна', a)
+# print('Цифра в позиции сотен равна', b)
+# print('Цифра в позиции десятков равна', c)
+# print('Цифра в позиции единиц равна', d)
+
+# print('*****************')
+# print('*', '*', sep='               ')
+# print('*', '*', sep='               ')
+# print('*****************')
+
+
+# a = int(input())
+# b = int(input())
+# print('Квадрат суммы 3 и 2 равен', (a+b)**2)
+# print('Сумма квадратов 3 и 2 равна', a**2+b**2)
+
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# d = int(input())
+# print(a**b + c**d)
+
+# a, b, c, d = int(input()), int(input()), int(input()), int(input())
+# print(a ** b + c ** d)
+
+# a = int(input())
+# b = a*10 + a
+# c = b*10 + a
+# print(a + b + c)
+
+
+# n = input()
+# print(int(n) + int(n * 2) + int(n * 3))
+
+
+# num = 1
+# print(int(num * 2))
+
+
+# if not [1, 2, 3, 4, 5, 6, 7]:
+#     print()
+# else:
+#     print('Ошибка значения')
+
+# num = int(input('Введите число от 1 до 7:'))
+# if 6 <= num <= 7:
+#     print('Выходной')
+# elif 0 < num < 6:
+#     print('Будний день')
+# else:
+#     print('Введено значение вне указанного промежутка')
+
+
+# a = int(input())
+# b = int(input())
+# list = [a + b]
+# if a > b:
+#     print(f'Число {a} больше числа {b}', end='Привет')
+# else:
+#     a < b
+#     print(f'Число {a} меньше числа {b}', end=list)
+
+
+# a = int(input())
+# b = int(input())
+# if a > b:
+#     print(f'большее {a}, меньшее{b}')
+# else:
+#     print(f'большее {b}, меньшее{a}')
+
+# num1 = int(input())
+# num2 = int(input())
+# if num2 == num1**2 or num1 == num2**2:
+#     print('Yes')
+# else:
+#     print('No')
--- 1.py
+++ 1.py
@@ -1,22 +1,106 @@
-def InputNumbers(inputText):
-    is_OK = False
-    while not is_OK:
-        try:
-            number = int(input(f"{inputText}"))
-            is_OK = True
-        except ValueError:
-            print("Это не число!")
-    return number
+
+"""
+1.1 Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Пример:
+- 6 -> да
+- 7 -> да
+- 1 -> нет
+"""
 
 
-def checkNumber(num):
-    if 6 <= num <= 7:
-        print("Выходной")
-    elif 0 < num < 6:
-        print("Нет")
-    else:
-        print("число вне пределов 7 дней")
+# num = int(input('Введите число день недели от 1 до 7:'))
+# if 6 <= num <= 7:
+#     print('Да')
+# elif 0 < num < 6:
+#     print('Нет')
+# else:
+#     print('Введено значение вне указанного промежутка')
 
 
-num = InputNumbers("Введите число: ")
-checkNumber(num)
+"""
+2.1 Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+"""
+
+
+# def inputNumbers(x):
+#     xyz = ["X", "Y", "Z"]
+#     a = []
+#     for i in range(x):
+#         a.append(input(f"Введите значение {xyz[i]}: "))
+#     return a
+
+
+# def checkPredicate(x):
+#     left = not (x[0] or x[1] or x[2])
+#     right = not x[0] and not x[1] and not x[2]
+#     result = left == right
+#     return result
+
+
+# statement = inputNumbers(3)
+
+# if checkPredicate(statement) == True:
+#     print(f"Утверждение истинно")
+# else:
+#     print(f"Утверждение ложно")
+
+
+"""
+2.2 Напишите программу, которая принимает на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка (или на какой оси она находится).
+
+Пример:
+
+- x=34; y=-30 -> 4
+- x=2; y=4-> 1
+- x=-34; y=-30 -> 3
+"""
+
+# x = int(input('Введите координаты точки x: '))
+# y = int(input('Введите координаты точки y: '))
+# if x > 0 and y > 0:
+#     print('1')
+# if x < 0 and y > 0:
+#     print('2')
+# if x < 0 and y < 0:
+#     print('3')
+# if x > 0 and y < 0:
+#     print('4')
+
+
+"""
+3.1 Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+"""
+
+# n = int(input('Номер четверти: '))
+# if n < 1 or n > 4:
+#     print('Пожалуйста, повторите ввод')
+# elif n == 1:
+#     print('x > 0 and y > 0')
+# elif n == 2:
+#     print('x < 0 and y > 0')
+# elif n == 3:
+#     print('x < 0 and y < 0')
+# elif n == 4:
+#     print('x > 0 and y < 0')
+
+
+"""
+3.2 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+Пример:
+
+- A (3,6); B (2,1) -> 5,09
+- A (7,-5); B (1,-1) -> 7,21
+"""
+# ФОРМУЛА AB = √(xb - xa)2 + (yb - ya)2
+
+# from math import sqrt
+# print('Укажите координаты точки А:')
+# x_A = float(input('X: '))
+# y_A = float(input('Y: '))
+# print('Укажите координаты точки B:')
+# x_B = float(input('X: '))
+# y_B = float(input('Y: '))
+
+# print('Расстояние от A до B: ', round(
+#     sqrt((x_A - x_B)**2 + (y_A - y_B)**2), 2))
